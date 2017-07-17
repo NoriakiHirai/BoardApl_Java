ec046428f9eab4253f31307cc0f53f8900ad01b7
NoriakiHirai <noriasrock@gmail.com>
2017-07-17 12:30:26 +0900

    第2回課題回答サンプルコメント追加

--- src/boardApp/Board.java
+++ src/boardApp/Board.java
@@ -5,16 +5,26 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Board {
-	private final String category;
-	public static String title = "掲示板";
-	private final String user;
+	/**
+	 * 掲示板の各機能を実装したクラス
+	 * 
+	 * @author guest
+	 */
+	private final String category; // 掲示板の種別
+	public static String TITLE = "掲示板";
+	private final String user; // 投稿者
 
+	/**
+	 * 掲示版の属性を設定するコンストラクタ
+	 * 
+	 * @param category 掲示板の種別
+	 * @param user 投稿者
+	 */
 	public Board(String category, String user) {
 		super();
 		this.category = category;
 		this.user = user;
 	}
-
 	public String getCategory() {
 		return category;
 	}
@@ -23,14 +33,21 @@ public class Board {
 		return user;
 	}
 
-	public List<String> Contribution() {
+	/**
+	 * 掲示板への投稿をコンソールから入力させる
+	 * 
+	 * @return List<String> 投稿内容を格納した配列
+	 */
+	public List<String> contribution() {
 		Boolean ctrbFlag = false;
 		String yes = "y";
 
 		List<String> contributions = new ArrayList<>();
+		// Scannerクラスを利用して，コンソールの標準入力の内容を取得する
 		Scanner scanner = new Scanner(System.in);
 
 		System.out.println(this.category + "掲示板に投稿してください。");
+		// 任意の回数、投稿を繰り返す
 		while (!ctrbFlag) {
 			System.out.println();
 			System.out.print("投稿内容 > ");
@@ -47,14 +64,19 @@ public class Board {
 		return contributions;
 	}
 	
-	public void Output(Board board, List<String> contributions) {
-		// 掲示板の表示
+	/**
+	 *  投稿された内容を掲示板に表示する
+	 *  	
+	 * @param board 掲示板(Board)クラスのインスタンスオブジェクト
+	 * @param contributions 投稿を格納した配列
+	 */
+	public void output(Board board, List<String> contributions) {
 		// タイトル部分の表示
 		System.out.println();
 		System.out.println("------------------------------");
 		System.out.println();
 		System.out.println("********************");
-		System.out.println(board.getCategory() + title);
+		System.out.println(board.getCategory() + TITLE);
 		System.out.println("********************");
 		
 		// 投稿内容部分の表示
--- src/boardApp/Main.java
+++ src/boardApp/Main.java
@@ -3,16 +3,26 @@ package boardApp;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * 掲示板への投稿(contribution)や投稿内容の掲示板への出力を実行するクラス
+ * 
+ * @author guest
+ */
 public class Main {
+	/**
+	 * 掲示板インスタンスの作成、掲示板への投稿、およびその出力を行う。
+	 * 
+	 * @param args 実行時引数。無視される。
+	 */
 	public static void main (String[] args) {
 		// インスタンスの作成
 		Board myBoard = new Board("news", "user1");
 				
 		// 投稿の実施
 		List<String> contributions = new ArrayList<>();
-		contributions = myBoard.Contribution();
+		contributions = myBoard.contribution();
 		
 		// 投稿内容の出力		
-		myBoard.Output(myBoard, contributions);
+		myBoard.output(myBoard, contributions);
 	}
 }
