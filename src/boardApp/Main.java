310a8a18090a1686ec15b8ab060783e71ce955e5
NoriakiHirai <noriasrock@gmail.com>
2017-08-12 20:19:13 +0900

    メインクラスのメソッドをメインメソッド1つに片寄せ

--- src/boardApp/Main.java
+++ src/boardApp/Main.java
@@ -8,8 +8,9 @@ import java.util.Scanner;
  * @author guest
  */
 public class Main {
-	
-	private static Scanner scanner; 
+
+	private static Scanner scanner;
+
 	/**
 	 * 掲示板への投稿、およびその出力に関する処理の制御を行う。
 	 * 
@@ -19,36 +20,6 @@ public class Main {
 	public static void main(String[] args) {
 		scanner = new Scanner(System.in);
 
-		// ユーザー名を取得する
-		String userName = requestUserInfo();
-
-		// 投稿情報オブジェクトを格納する配列
-		// 当課題では3件のみ投稿を受け付けるので、配列の長さは3とする
-		ContributionInfo[] ciList = new ContributionInfo[3];
-
-		// 投稿処理(3件)
-		// 投稿文を取得し、投稿情報インスタンスを作成する。
-		// 作成したインスタンスは、投稿情報配列に保持しておく。
-		for (int i = 0; i < 3; i++) {
-			String sentence = requestContribution();
-			// 投稿情報インスタンスの生成
-			ContributionInfo contributionInfo = new ContributionInfo(userName, sentence);
-			ciList[i] = contributionInfo;
-		}
-		// 掲示板生成処理
-		showBoard(ciList);
-
-		scanner.close();
-	}
-
-	/**
-	 * 掲示板アプリの初期処理として、ユーザーの名前をコンソールから取得する
-	 * 
-	 * @param sc
-	 *            スキャナーオブジェクト
-	 * @return userName ユーザー名
-	 */
-	private static String requestUserInfo() {
 		System.out.println("*** 第二回課題掲示板へようこそ ***");
 
 		// ユーザー名の入力
@@ -56,30 +27,24 @@ public class Main {
 		System.out.print("ユーザー名:");
 		String userName = scanner.nextLine();
 
-		return userName;
-	}
-
-	/**
-	 * 掲示板への投稿をコンソールから入力させる
-	 * 
-	 * @return sentence 投稿文
-	 */
-	private static String requestContribution() {
+		// 投稿情報オブジェクトを格納する配列
+		// 当課題では3件のみ投稿を受け付けるので、配列の長さは3とする
+		ContributionInfo[] ciList = new ContributionInfo[3];
+		
 		// 投稿文の入力
-		System.out.println("投稿を書き込んでください。");
-		System.out.print("投稿内容 > ");
-		String sentence = scanner.nextLine();
+		// 投稿処理(3件)
+		// 投稿文を取得し、投稿情報インスタンスを作成する。
+		// 作成したインスタンスは、投稿情報配列に保持しておく。
+		for (int i = 0; i < 3; i++) {
+			System.out.println("投稿を書き込んでください。");
+			System.out.print("投稿内容 > ");
+			String sentence = scanner.nextLine();
 
-		return sentence;
-	}
+			// 投稿情報インスタンスの生成
+			ContributionInfo contributionInfo = new ContributionInfo(userName, sentence);
+			ciList[i] = contributionInfo;
+		}
 
-	/**
-	 * 掲示板を生成し、投稿された内容を掲示板に表示する
-	 * 
-	 * @param ciList
-	 *            投稿情報オブジェクトの配列
-	 */
-	private static void showBoard(ContributionInfo[] ciList) {
 		// タイトル部分の表示
 		System.out.println();
 		System.out.println("------------------------------");
@@ -94,5 +59,7 @@ public class Main {
 			System.out.println(ciList[i].getSentence());
 			System.out.println();
 		}
+
+		scanner.close();
 	}
 }
